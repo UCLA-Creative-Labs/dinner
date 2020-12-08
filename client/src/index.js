1c786845391399d2c123439a85145b1cd36125e5
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-12-07 20:47:57 -0500

    Fixed networking, added basic JS

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,13 @@
-console.log(document.querySelector('a-entity'));
-console.log(AFRAME.scenes[0]);
+const MAIN_SCENE = AFRAME.scenes[0];
+const PLAYER = document.querySelector('#player');
+
+function setupNetworking() {
+  const serverUrl = process.env.NODE_ENV === 'production' ? 'TODO' : 'http://localhost:3000';
+  const shouldDebug = process.env.NODE_ENV === 'production' ? false : true;
+  MAIN_SCENE.setAttribute('networked-scene', {
+    'serverURL': serverUrl,
+    'debug': shouldDebug
+  });
+}
+
+setupNetworking();
--- server/src/index.js
+++ server/src/index.js
@@ -5,14 +5,67 @@ const socketIo = require('socket.io');
 const logger = require('./logger');
 
 const app = express();
-app.use(cors({
-  'origin': 'http://localhost:8080'
-}));
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: [ 'GET', 'POST' ],
+    credentials: true
+  }
+});
+
+const rooms = {};
 
 io.on('connection', (socket) => {
   socket.emit('handshake', 'Recieved connection.');
+  logger.info('Received connection');
+
+  let curRoom = null;
+
+  socket.on("joinRoom", data => {
+    const { room } = data;
+
+    if (!rooms[room]) {
+      rooms[room] = {
+        name: room,
+        occupants: {}
+      };
+    }
+
+    const joinedTime = Date.now();
+    rooms[room].occupants[socket.id] = joinedTime;
+    curRoom = room;
+
+    logger.info(`${socket.id} joined room ${room}`);
+    socket.join(room);
+
+    socket.emit('connectSuccess', { joinedTime });
+    const occupants = rooms[room].occupants;
+    io.in(curRoom).emit('occupantsChanged', { occupants });
+  });
+
+  socket.on('send', data => {
+    io.to(data.to).emit('send', data);
+  });
+
+  socket.on('broadcast', data => {
+    socket.to(curRoom).broadcast.emit('broadcast', data);
+  });
+
+  socket.on('disconnect', () => {
+    if (rooms[curRoom]) {
+      logger.info(`${socket.id} disconnected from ${curRoom}`);
+
+      delete rooms[curRoom].occupants[socket.id];
+      const occupants = rooms[curRoom].occupants;
+      socket.to(curRoom).broadcast.emit('occupantsChanged', { occupants });
+
+      if (occupants == {}) {
+        logger.info(`Everyone left`);
+        delete rooms[curRoom]
+      }
+    }
+  });
 });
 
 const port = 3000;
